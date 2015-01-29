b9eb9dbea9fdbbf6b87ea579950c829934b0f839
benrmatthews <ben@montfort.io>
2015-01-29 06:25:00 +0000

    Fixed sign up issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,11 +8,11 @@ ReachScout::Application.configure do
   ActionMailer::Base.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    :address        => 'smtp.gmail.com',
+    :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['GMAIL_USERNAME'],
-    :password       => ENV['GMAIL_PASSWORD'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
   }
 
